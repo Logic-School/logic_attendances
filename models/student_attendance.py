0cf26d9683baf27e20f7ec5d13ccd8e872653657
argenspin <argenspin@gmail.com>
2023-10-12 18:04:41 +0530

    added some fields to tree view from base

--- models/student_attendance.py
+++ models/student_attendance.py
@@ -7,6 +7,12 @@ class StudentAttendance(models.Model):
     session_id = fields.Many2one('attendance.session',string="Attendance Session",store=True)
     class_id = fields.Many2one('logic.base.class',related="session_id.class_id",store=True)
     # batch_id = fields.Many2one('logic.base.batch',relate="session_id.batch_id")
+    attempt = fields.Selection(related="student_id.attempt")
+    recording_status = fields.Selection(related="student_id.recording_status")
+    email = fields.Char(related="student_id.email")
+    phone_number = fields.Char(related="student_id.phone_number")
+    parent_whatsapp = fields.Char(related="student_id.parent_whatsapp")
+
     date = fields.Date(related='session_id.date')
     morning_attendance = fields.Boolean(string="Morning Attendance")
     evening_attendance = fields.Boolean(string="Evening Attendance")
